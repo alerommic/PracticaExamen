c718a5c705d2077f761a253ffa538fe5d088a62b
Alex Romaguera <alerommic@alu.edu.gva.es>
2023-02-08 08:38:19 +0100

    Commit de la cosa esta para practica

---
+++ gitpracticar/src/gitpracticar/EseJuan.java
@@ -0,0 +1,15 @@
+package gitpracticar;
+
+import java.util.Scanner;
+
+public class EseJuan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		for (int i = 0; i < 5; i++) {
+			System.out.println("hola juan");
+			
+		}
+	}
+
+}
